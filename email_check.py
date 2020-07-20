3d489a2cecd05ef8f103dbaa527dd41f038c35f8
shutterberg <misquithin@gamil.com>
2020-07-20 14:57:10 +0530

    print added

--- email_check.py
+++ email_check.py
@@ -1,9 +1,12 @@
 
 #check conditions if entered email is a string if yes print the same using regex
 import re
+print("enter your email")
 email=input()
 list=re.findall('(?i)\S+@\S+.\S+',email) #\S+ --non space charcters #(?i) case sensitive removed
 if(len(list)==1):
     print(list[0].lower())        #brings evrything to lowerCASE
 else:
     print("Invalid email Id")      #error_message
+
+
